991c62a7cf3b806180ba02b5bfb2305c9fe710d6
CaptTrews <capttrews@gmail.com>
2020-01-21 13:45:22 +0000

    Updated from network content collector
    Signed-off-by: CaptTrews <capttrews@gmail.com>

---
+++ tests/unit/mock/path.py
@@ -0,0 +1,9 @@
+from ansible_collections.junipernetworks.junos.tests.unit.compat.mock import (
+    MagicMock,
+)
+from ansible.utils.path import unfrackpath
+
+
+mock_unfrackpath_noop = MagicMock(
+    spec_set=unfrackpath, side_effect=lambda x, *args, **kwargs: x
+)
