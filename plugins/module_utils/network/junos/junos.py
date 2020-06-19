e8584364cba8ac1ecf8fe3f6c422390f35a6bb21
Rohit Thakur <rohitthakur2590@outlook.com>
2020-06-19 22:15:20 +0530

    doc changes
    Signed-off-by: Rohit Thakur <rohitthakur2590@outlook.com>

--- plugins/module_utils/network/junos/junos.py
+++ plugins/module_utils/network/junos/junos.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 #
-# pylint: skip-file
 import collections
 import json
 from contextlib import contextmanager
