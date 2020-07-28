fc55b82a8c8069b8b6292e8508cf5c4170cc7436
Nilashish Chakraborty <nilashishchakraborty8@gmail.com>
2020-07-28 12:38:34 +0530

    Handle src & backup properly when module alias is used (#83)
    Handle src & backup properly when module alias is used
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- plugins/action/junos.py
+++ plugins/action/junos.py
@@ -44,7 +44,9 @@ class ActionModule(ActionNetworkModule):
         del tmp  # tmp no longer has any effect
 
         module_name = self._task.action.split(".")[-1]
-        self._config_module = True if module_name == "junos_config" else False
+        self._config_module = (
+            True if module_name in ["junos_config", "config"] else False
+        )
         persistent_connection = self._play_context.connection.split(".")[-1]
         warnings = []
 
