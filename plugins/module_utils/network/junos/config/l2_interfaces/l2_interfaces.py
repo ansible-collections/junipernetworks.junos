eda13fc3a7d4899bc53bc1f3e4b334d1c3e24190
Rohit Thakur <rohitthakur2590@outlook.com>
2020-08-28 16:49:40 +0530

    parsed and rendered added
    Signed-off-by: Rohit Thakur <rohitthakur2590@outlook.com>

--- plugins/module_utils/network/junos/config/l2_interfaces/l2_interfaces.py
+++ plugins/module_utils/network/junos/config/l2_interfaces/l2_interfaces.py
@@ -152,7 +152,10 @@ class L2_interfaces(ConfigBase):
         """
         root = build_root_xml_node("interfaces")
         state = self._module.params["state"]
-        if state in ("merged", "replaced", "overridden") and not want:
+        if (
+            state in ("merged", "replaced", "overridden", "rendered")
+            and not want
+        ):
             self._module.fail_json(
                 msg="value of config parameter must not be empty for state {0}".format(
                     state
