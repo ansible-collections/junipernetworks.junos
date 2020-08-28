616ff75c6b7d0b4f55eaf3ff5fd830f4199466ad
Rohit Thakur <rohitthakur2590@outlook.com>
2020-08-28 16:45:05 +0530

    cosmetic changes
    Signed-off-by: Rohit Thakur <rohitthakur2590@outlook.com>

--- plugins/module_utils/network/junos/config/interfaces/interfaces.py
+++ plugins/module_utils/network/junos/config/interfaces/interfaces.py
@@ -145,7 +145,10 @@ class Interfaces(ConfigBase):
         """
         root = build_root_xml_node("interfaces")
         state = self._module.params["state"]
-        if state in ("merged", "replaced", "overridden") and not want:
+        if (
+            state in ("merged", "replaced", "overridden", "rendered")
+            and not want
+        ):
             self._module.fail_json(
                 msg="value of config parameter must not be empty for state {0}".format(
                     state
--- plugins/modules/junos_interfaces.py
+++ plugins/modules/junos_interfaces.py
@@ -95,7 +95,7 @@ options:
     description:
     - This option is used only with state I(parsed).
     - The value of this option should be the output received from the Junos device
-      by executing the command B(show running-config interface).
+      by executing the command B(show interfaces).
     - The state I(parsed) reads the configuration from C(running_config) option and
       transforms it into Ansible structured data as per the resource module's argspec
       and the value is then returned in the I(parsed) key within the result.
