5251eedf800ad199297799e8920d15e1af8ef11d
Rohit Thakur <rohitthakur2590@outlook.com>
2021-11-10 16:31:43 +0530

    autogenerated code
    Signed-off-by: Rohit Thakur <rohitthakur2590@outlook.com>

---
+++ plugins/module_utils/network/junos/config/snmp_server/snmp_server.py
@@ -0,0 +1,156 @@
+#
+# -*- coding: utf-8 -*-
+# Copyright 2021 Red Hat
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
+The junos_snmp_server class
+It is in this file where the current configuration (as dict)
+is compared to the provided configuration (as dict) and the command set
+necessary to bring the current configuration to it's desired end-state is
+created
+"""
+from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.cfg.base import (
+    ConfigBase,
+)
+from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import (
+    to_list,
+)
+from ansible_collections.junipernetworks.junos.plugins.module_utils.network.junos.facts.facts import Facts
+
+
+class Snmp_server(ConfigBase):
+    """
+    The junos_snmp_server class
+    """
+
+    gather_subset = [
+        '!all',
+        '!min',
+    ]
+
+    gather_network_resources = [
+        'snmp_server',
+    ]
+
+    def __init__(self, module):
+        super(Snmp_server, self).__init__(module)
+
+    def get_snmp_server_facts(self):
+        """ Get the 'facts' (the current configuration)
+
+        :rtype: A dictionary
+        :returns: The current configuration as a dictionary
+        """
+        facts, _warnings = Facts(self._module).get_facts(self.gather_subset, self.gather_network_resources)
+        snmp_server_facts = facts['ansible_network_resources'].get('snmp_server')
+        if not snmp_server_facts:
+            return []
+        return snmp_server_facts
+
+    def execute_module(self):
+        """ Execute the module
+
+        :rtype: A dictionary
+        :returns: The result from module execution
+        """
+        result = {'changed': False}
+        warnings = list()
+        commands = list()
+
+        existing_snmp_server_facts = self.get_snmp_server_facts()
+        commands.extend(self.set_config(existing_snmp_server_facts))
+        if commands:
+            if not self._module.check_mode:
+                self._connection.edit_config(commands)
+            result['changed'] = True
+        result['commands'] = commands
+
+        changed_snmp_server_facts = self.get_snmp_server_facts()
+
+        result['before'] = existing_snmp_server_facts
+        if result['changed']:
+            result['after'] = changed_snmp_server_facts
+
+        result['warnings'] = warnings
+        return result
+
+    def set_config(self, existing_snmp_server_facts):
+        """ Collect the configuration from the args passed to the module,
+            collect the current configuration (as a dict from facts)
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        want = self._module.params['config']
+        have = existing_snmp_server_facts
+        resp = self.set_state(want, have)
+        return to_list(resp)
+
+    def set_state(self, want, have):
+        """ Select the appropriate function based on the state provided
+
+        :param want: the desired configuration as a dictionary
+        :param have: the current configuration as a dictionary
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        state = self._module.params['state']
+        if state == 'overridden':
+            kwargs = {}
+            commands = self._state_overridden(**kwargs)
+        elif state == 'deleted':
+            kwargs = {}
+            commands = self._state_deleted(**kwargs)
+        elif state == 'merged':
+            kwargs = {}
+            commands = self._state_merged(**kwargs)
+        elif state == 'replaced':
+            kwargs = {}
+            commands = self._state_replaced(**kwargs)
+        return commands
+    @staticmethod
+    def _state_replaced(**kwargs):
+        """ The command generator when state is replaced
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        commands = []
+        return commands
+
+    @staticmethod
+    def _state_overridden(**kwargs):
+        """ The command generator when state is overridden
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        commands = []
+        return commands
+
+    @staticmethod
+    def _state_merged(**kwargs):
+        """ The command generator when state is merged
+
+        :rtype: A list
+        :returns: the commands necessary to merge the provided into
+                  the current configuration
+        """
+        commands = []
+        return commands
+
+    @staticmethod
+    def _state_deleted(**kwargs):
+        """ The command generator when state is deleted
+
+        :rtype: A list
+        :returns: the commands necessary to remove the current configuration
+                  of the provided objects
+        """
+        commands = []
+        return commands
