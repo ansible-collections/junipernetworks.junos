b2c5ae7db328bba55bdbe226b869d7e89275ac5f
Gonéri Le Bouder <goneri@lebouder.net>
2022-03-10 14:01:03 -0500

    DNM - CI TESTING
    Depends-On: https://github.com/ansible/ansible-zuul-jobs/pull/1386

--- plugins/modules/junos_package.py
+++ plugins/modules/junos_package.py
@@ -4,11 +4,6 @@
 # (c) 2017, Ansible by Red Hat, inc
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import absolute_import, division, print_function
-
-__metaclass__ = type
-
-
 DOCUMENTATION = """
 module: junos_package
 author: Peter Sprygada (@privateip)
