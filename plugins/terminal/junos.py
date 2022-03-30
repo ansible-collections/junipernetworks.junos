b870681705207e50ceb3751fdb7ca9edb1950d2b
Pranav Bhatt <adpranavb2000@gmail.com>
2022-03-30 16:33:24 +0530

    bumped black
    Signed-off-by: Pranav Bhatt <adpranavb2000@gmail.com>

--- plugins/terminal/junos.py
+++ plugins/terminal/junos.py
@@ -23,6 +23,7 @@ __metaclass__ = type
 
 import re
 
+from ansible.module_utils.common.text.converters import to_bytes
 from ansible.plugins.terminal import TerminalBase
 from ansible.errors import AnsibleConnectionFailure
 from ansible.utils.display import Display
@@ -33,13 +34,15 @@ display = Display()
 class TerminalModule(TerminalBase):
 
     terminal_stdout_re = [
-        re.compile(rb"({primary:node\d+})?[\r\n]?[\w@+\-\.:\/\[\]]+[>#%] ?$")
+        re.compile(
+            to_bytes(r"({primary:node\d+})?[\r\n]?[\w@+\-\.:\/\[\]]+[>#%] ?$")
+        )
     ]
 
     terminal_stderr_re = [
-        re.compile(rb"unknown command"),
-        re.compile(rb"syntax error"),
-        re.compile(rb"[\r\n]error:"),
+        re.compile(to_bytes(r"unknown command")),
+        re.compile(to_bytes(r"syntax error")),
+        re.compile(to_bytes(r"[\r\n]error:")),
     ]
 
     terminal_config_prompt = re.compile(r"^.+#$")
