84dc279d0967e1f5a11b0b7883fb1090513d2781
Stephen Boyd <stephen.boyd@sohonet.com>
2023-09-27 14:18:46 -0700

    Formatting

--- plugins/module_utils/network/junos/facts/security_zones/security_zones.py
+++ plugins/module_utils/network/junos/facts/security_zones/security_zones.py
@@ -226,7 +226,9 @@ class Security_zonesFacts(object):
                         temp_sec_zone["address_book"]["address_sets"] = []
 
                         if isinstance(security_zone["address-book"]["address-set"], dict):
-                            security_zone["address-book"]["address-set"] = [security_zone["address-book"]["address-set"]]
+                            security_zone["address-book"]["address-set"] = [
+                                security_zone["address-book"]["address-set"]
+                            ]
 
                         for address_set in security_zone["address-book"]["address-set"]:
                             temp_address_set = {}
