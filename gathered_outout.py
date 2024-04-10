b458e574bf79260452c07b5b61d985491d72a1ad
Vinay Mulugund <roverflow5@gmail.com>
2024-04-10 13:33:52 +0530

    fix lint and test function name

---
+++ gathered_outout.py
@@ -0,0 +1,1 @@
+[{'access': {'vlan': 'vlan100'}, 'name': 'ge-0/0/1', 'unit': 0, 'enhanced_layer': True}, {'name': 'ge-0/0/2', 'trunk': {'allowed_vlans': ['vlan200', 'vlan300'], 'native_vlan': '400'}, 'unit': 0, 'enhanced_layer': True}]
